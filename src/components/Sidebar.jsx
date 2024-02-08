3e8caa496a88ca3f0719dd3df4a4235b374855c3
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2024-02-09 11:55:31 +1300

    Sidebar with Listbutton, icons, switch

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,89 @@
-import { Box, List, ListItem,ListItemButton,ListItemIcon, ListItemText, } from "@mui/material"
+import { Box, List, ListItem,ListItemButton,ListItemIcon, ListItemText,Switch } from "@mui/material"
+import HomeIcon from "@mui/icons-material/Home";
+import Person2Icon from "@mui/icons-material/Person2";
+import ArticleIcon from "@mui/icons-material/Article";
+import SettingsIcon from "@mui/icons-material/Settings";
+import GroupIcon from "@mui/icons-material/Group";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import ModeNightIcon from "@mui/icons-material/ModeNight";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 
 export const Sidebar = () => {
   return (
-    <Box bgcolor='skyblue' flex={1} p={2} xs={{display: {xs: "none", sm:"block"}}}>
-      
+    <Box
+      bgcolor="none"
+      flex={1}
+      p={2}
+      xs={{ display: { xs: "none", sm: "block" } }}
+    >
       <List>
-          <ListItem disablePadding>
-            <ListItemButton compo>
-              <ListItemIcon>
-            
-              </ListItemIcon>
-              <ListItemText primary="Homepage" />
-            </ListItemButton>
-          </ListItem>
-          </List>
-          
-          </Box>
-  )
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#pages">
+            <ListItemIcon>
+              <ArticleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#groups">
+            <ListItemIcon>
+              <GroupIcon />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#marketp=lace">
+            <ListItemIcon>
+              <StorefrontIcon />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#friends">
+            <ListItemIcon>
+              <FavoriteBorderIcon />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#setting">
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Setting" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#profile">
+            <ListItemIcon>
+              <Person2Icon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <ModeNightIcon />
+            </ListItemIcon>
+            <Switch  />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
 }
