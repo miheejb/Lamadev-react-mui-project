53a090fbe7cf43fe2fcd6446911416e972275496
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2024-02-08 23:06:08 +1300

    setup sidebar,rightbar,feed, nav.jsx

---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export const Rightbar = () => {
+  return (
+    <Box bgcolor="lightcoral" flex={2} p={2}>
+      Rightbar
+    </Box>
+  );
+}
