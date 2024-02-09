1bf0dded69bad20eb13e342c21ea1f7574afa58d
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2024-02-09 13:12:11 +1300

    Avatargrouop and Avatar in Rightbar.jsx

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material"
+import { Avatar, AvatarGroup, Box, Typography } from "@mui/material"
 import React from "react"
 
 export const Rightbar = () => {
@@ -9,8 +9,19 @@ export const Rightbar = () => {
       p={2}
       xs={{ display: { xs: "none", sm: "block" } }}
     >
-      <Box position="fixed"> rightbar </Box>
-      
+      <Box position="fixed">
+        <Typography variant="h6" fontWeight={100}>
+          {" "}
+          Online Friends{" "}
+        </Typography>
+        <AvatarGroup max={4}>
+          <Avatar  alt="Remy Sharp" src="/assets/images/avatar1.svg" />
+          <Avatar alt="Travis Howard" src="/assets/images/avatar2.svg" />
+          <Avatar alt="Cindy Baker" src="/assets/images/avatar3.svg"  />
+          <Avatar alt="Agnes Walker" src="/assets/images/avatar4.svg"  />
+          <Avatar alt="Trevor Henderson" src="/assets/images/avatar5.svg" />
+        </AvatarGroup>
+      </Box>
     </Box>
   );
 }
